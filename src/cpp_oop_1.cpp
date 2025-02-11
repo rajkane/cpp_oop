75705dfc0df0c6e6a1654a8d0f5144ed6054d2f0
Daniel Torac <daniel.torac@gmail.com>
2025-02-12 00:21:07 +0100

    update cpp_oop_2 example for encapsulation

--- src/cpp_oop_1.cpp
+++ src/cpp_oop_1.cpp
@@ -2,6 +2,8 @@
 
 using namespace std;
 
+// example for class and object
+
 class Taxonom {
 public:
     string domain;
@@ -16,30 +18,30 @@ public:
     string species;
 };
 
-int main()
-{
-    Taxonom taxonom;
-    taxonom.domain = "Eucaryota";
-    taxonom.kingdom = "Animalia";
-    taxonom.phylum = "Chordata";
-    taxonom.clas = "Mammalia";
-    taxonom.order = "Carnivora";
-    taxonom.suborder = "Feliformia";
-    taxonom.family = "Felidae";
-    taxonom.subfamily = "Phantherinae";
-    taxonom.genus = "Panthera";
-    taxonom.species = "P.tigris";
+// int main()
+// {
+//     Taxonom taxonom;
+//     taxonom.domain = "Eucaryota";
+//     taxonom.kingdom = "Animalia";
+//     taxonom.phylum = "Chordata";
+//     taxonom.clas = "Mammalia";
+//     taxonom.order = "Carnivora";
+//     taxonom.suborder = "Feliformia";
+//     taxonom.family = "Felidae";
+//     taxonom.subfamily = "Phantherinae";
+//     taxonom.genus = "Panthera";
+//     taxonom.species = "P.tigris";
 
-    cout << "Domain: " << taxonom.domain << endl;
-    cout << "Kingdom: " << taxonom.kingdom << endl;
-    cout << "Phylum: " << taxonom.phylum << endl;
-    cout << "Class: " << taxonom.clas << endl;
-    cout << "Order: " << taxonom.order << endl;
-    cout << "Suborder: " << taxonom.suborder << endl;
-    cout << "Family: " << taxonom.family << endl;
-    cout << "Subfamily: " << taxonom.subfamily << endl;
-    cout << "Genus: " << taxonom.genus << endl;
-    cout << "Species: " << taxonom.species << endl;
+//     cout << "Domain: " << taxonom.domain << endl;
+//     cout << "Kingdom: " << taxonom.kingdom << endl;
+//     cout << "Phylum: " << taxonom.phylum << endl;
+//     cout << "Class: " << taxonom.clas << endl;
+//     cout << "Order: " << taxonom.order << endl;
+//     cout << "Suborder: " << taxonom.suborder << endl;
+//     cout << "Family: " << taxonom.family << endl;
+//     cout << "Subfamily: " << taxonom.subfamily << endl;
+//     cout << "Genus: " << taxonom.genus << endl;
+//     cout << "Species: " << taxonom.species << endl;
 
-    return 0;
-}
+//     return 0;
+// }
---
+++ src/cpp_oop_2.cpp
@@ -0,0 +1,59 @@
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+// example for encapsulation
+
+class Taxonom {
+private:
+    string domain;
+    string kingdom;
+    string phylum;
+    string clas;
+    string order;
+    string suborder;
+    string family;
+    string subfamily;
+    string genus;
+    string species;
+
+public:
+    Taxonom(string domain, string kingdom, string phylum,
+            string clas, string order, string suborder, string family,
+            string subfamily, string genus, string species){
+        this->domain = domain;
+        this->kingdom = kingdom;
+        this->phylum = phylum;
+        this->clas = clas;
+        this->order = order;
+        this->suborder = suborder;
+        this->family = family;
+        this->subfamily = subfamily;
+        this->genus = genus;
+        this->species = species;
+    }
+
+    void get_info(){
+        cout << "Domain: " << domain << endl;
+        cout << "Kingdom: " << kingdom << endl;
+        cout << "Phylum: " << phylum << endl;
+        cout << "Class: " << clas << endl;
+        cout << "Order: " << order << endl;
+        cout << "Suborder: " << suborder << endl;
+        cout << "Family: " << family << endl;
+        cout << "Subfamily: " << subfamily << endl;
+        cout << "Genus: " << genus << endl;
+        cout << "Species: " << species << endl;
+    }
+};
+
+int main()
+{
+    Taxonom taxonom("Eucaryota", "Animalia", "Chordata", "Mammalia", "Carnivora",
+        "Feliformia", "Felidae", "Phantherinae", "Panthera", "P.tigris");
+
+    taxonom.get_info();
+
+    return 0;
+}
