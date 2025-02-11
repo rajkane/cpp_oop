394f591e3e628e540f1fc224e352e5783349e2ad
Daniel Torac <daniel.torac@gmail.com>
2025-02-11 23:07:34 +0100

    update cpp_oop_1

--- src/cpp_oop_1.cpp
+++ src/cpp_oop_1.cpp
@@ -2,8 +2,44 @@
 
 using namespace std;
 
+class Taxonom {
+public:
+    string domain;
+    string kingdom;
+    string phylum;
+    string clas;
+    string order;
+    string suborder;
+    string family;
+    string subfamily;
+    string genus;
+    string species;
+};
+
 int main()
 {
-    cout << "Hello World!" << endl;
+    Taxonom taxonom;
+    taxonom.domain = "Eucaryota";
+    taxonom.kingdom = "Animalia";
+    taxonom.phylum = "Chordata";
+    taxonom.clas = "Mammalia";
+    taxonom.order = "Carnivora";
+    taxonom.suborder = "Feliformia";
+    taxonom.family = "Felidae";
+    taxonom.subfamily = "Phantherinae";
+    taxonom.genus = "Panthera";
+    taxonom.species = "P.tigris";
+
+    cout << "Domain: " << taxonom.domain << endl;
+    cout << "Kingdom: " << taxonom.kingdom << endl;
+    cout << "Phylum: " << taxonom.phylum << endl;
+    cout << "Class: " << taxonom.clas << endl;
+    cout << "Order: " << taxonom.order << endl;
+    cout << "Suborder: " << taxonom.suborder << endl;
+    cout << "Family: " << taxonom.family << endl;
+    cout << "Subfamily: " << taxonom.subfamily << endl;
+    cout << "Genus: " << taxonom.genus << endl;
+    cout << "Species: " << taxonom.species << endl;
+
     return 0;
 }
